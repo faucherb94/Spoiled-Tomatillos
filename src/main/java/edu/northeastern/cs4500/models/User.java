89bbec8640b72ffb667b9765410b11d66a482d60
Joseph Donovan <joeydonovan4@gmail.com>
2018-02-19 17:18:35 -0500

    TEAM26-60: Adding getters/setters/constructors for User
    - These are needed in order to properly create a User via the POST call.

--- src/main/java/edu/northeastern/cs4500/models/User.java
+++ src/main/java/edu/northeastern/cs4500/models/User.java
@@ -27,18 +27,58 @@ public class User {
     @Column(unique = true)
     private String username;
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
     @NotBlank
     private String firstName;
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
     @NotBlank
     private String lastName;
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
 
     @NotBlank
     @Column(unique = true)
     private String email;
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
     @NotBlank
     private String role;
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
 
     @Lob
     @Column(name = "DisplayPicture")
@@ -53,4 +93,15 @@ public class User {
     @Temporal(TemporalType.TIMESTAMP)
     @LastModifiedDate
     private Date updatedAt;
+
+    public User(String username, String firstName, String lastName, String email,
+                String role) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+    }
+
+    public User() {}
 }
