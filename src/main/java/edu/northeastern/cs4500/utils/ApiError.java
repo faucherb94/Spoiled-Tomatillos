a2e684ccaa34a20f3dd95223b9e5c59348d2e3f5
Joseph Donovan <joeydonovan4@gmail.com>
2018-03-11 16:47:56 -0400

    TEAM26-94: API Error Model
    - Create a model for error handling based on the code in this link:
      https://www.toptal.com/java/spring-boot-rest-api-error-handling
    - The FE can use this to detect errors and display them to the user.
    - The Exception Handler class is used to handle certain thrown
    exceptions in any particular way (e.g. return a corresponding HTTP code
    and make everything look nicer)

---
+++ src/main/java/edu/northeastern/cs4500/utils/ApiError.java
@@ -0,0 +1,64 @@
+package edu.northeastern.cs4500.utils;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.annotation.JsonTypeIdResolver;
+import com.fasterxml.jackson.databind.jsontype.impl.TypeIdResolverBase;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+@JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.CUSTOM,
+        property = "error", visible = true)
+@JsonTypeIdResolver(LowerCaseClassNameResolver.class)
+class ApiError {
+
+    private HttpStatus status;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+    private String message;
+    private String debugMessage;
+
+    private ApiError() {
+        this.timestamp = LocalDateTime.now();
+    }
+
+    ApiError(HttpStatus status) {
+        this();
+        this.status = status;
+    }
+
+    ApiError(HttpStatus status, Throwable ex) {
+        this(status);
+        this.message = "Unexpected error";
+        this.debugMessage = ex.getLocalizedMessage();
+    }
+
+    ApiError(HttpStatus status, String message, Throwable ex) {
+        this(status);
+        this.message = message;
+        this.debugMessage = ex.getLocalizedMessage();
+    }
+}
+
+class LowerCaseClassNameResolver extends TypeIdResolverBase {
+
+    @Override
+    public String idFromValue(Object value) {
+        return value.getClass().getSimpleName().toLowerCase();
+    }
+
+    @Override
+    public String idFromValueAndType(Object value, Class<?> suggestedType) {
+        return idFromValue(value);
+    }
+
+    @Override
+    public JsonTypeInfo.Id getMechanism() {
+        return JsonTypeInfo.Id.CUSTOM;
+    }
+}
---
+++ src/main/java/edu/northeastern/cs4500/utils/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,32 @@
+package edu.northeastern.cs4500.utils;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    protected ResponseEntity<Object> handleDataIntegrityViolation(
+            DataIntegrityViolationException ex) {
+
+        if (ex.getCause() instanceof ConstraintViolationException) {
+            return buildResponseEntity(new ApiError(HttpStatus.CONFLICT,
+                    "Database error",
+                    ex.getRootCause()));
+        }
+        return buildResponseEntity(new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex));
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+}
