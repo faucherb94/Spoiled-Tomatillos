09dcdc6322448d70c4eecba5207027218adbb910
Joseph Donovan <joeydonovan4@gmail.com>
2018-03-12 19:17:33 -0400

    TEAM26-94: Add a service layer for Users
    - After reading online, apparently it's better to have a service layer
    that handles the "business logic" in between the repository and the
    presentation layers.

---
+++ src/main/java/edu/northeastern/cs4500/services/IUserService.java
@@ -0,0 +1,15 @@
+package edu.northeastern.cs4500.services;
+
+import edu.northeastern.cs4500.models.User;
+
+public interface IUserService {
+
+    User findByID(int id);
+
+    User findByUsername(String username);
+
+    User create(User u);
+
+    User update(int id, User u);
+
+}
