e6dd844d9c5023305a4335bef2323e446aa8b1d2
Maddy Leger <maddyleger1@gmail.com>
2018-03-29 09:54:02 -0400

    probably things working now

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -37,7 +37,7 @@ function createUser(newuser) {
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    /**var
+
     var email = profile.getEmail();
     var username = email.split("@")[0];
     var user = {
@@ -46,10 +46,10 @@ function onSignIn(googleUser) {
         propic: profile.getImageUrl(),
         email: email,
         username: username
-    } **/
+    }
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    // loginClick(user)
+    loginClick(user);
 }
